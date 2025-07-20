147747a38bdc420b88947fd3ea24d6e27df6a478
nwerosama <96593068+nwerosama@users.noreply.github.com>
2025-07-20 11:34:28 +1000

    Fix panic on empty DmModal response

--- src/events/interaction.rs
+++ src/events/interaction.rs
@@ -1,5 +1,9 @@
 use {
   crate::errors::BotError,
+  asahi::{
+    error,
+    warn
+  },
   poise::serenity_prelude::{
     Context,
     CreateInteractionResponseFollowup,
@@ -31,26 +35,34 @@ pub async fn on_interaction_create(
         .custom_id
         .strip_prefix("dm-")
         .and_then(|id| id.parse::<u64>().ok())
-        .ok_or_else(|| BotError::from("Component collected an invalid UserID!"))?;
+        .ok_or_else(|| BotError::from("Component stored an invalid UserID!"))?;
 
       let data = poise::execute_modal_on_component_interaction::<DmModal>(ctx, component.clone(), None, None).await?;
 
+      let mod_reply = match data {
+        Some(d) => d.mod_reply,
+        None => {
+          warn!("Modal passed empty value, not sending response back to user!");
+          return Ok(())
+        }
+      };
+
       UserId::new(uid)
-        .dm(
-          &ctx.http,
-          CreateMessage::new().content(format!("You have a new message!\n> {}", data.unwrap().mod_reply))
-        )
+        .dm(&ctx.http, CreateMessage::new().content(format!("You have a new message!\n> {mod_reply}")))
         .await?;
 
       component
-        .create_followup(&ctx.http, CreateInteractionResponseFollowup::new().content("Sent your response back!"))
+        .create_followup(
+          &ctx.http,
+          CreateInteractionResponseFollowup::new().content(format!("Sent your response back!\n> {mod_reply}"))
+        )
         .await?;
 
       Ok::<(), BotError>(())
     }
     .await
   {
-    asahi::error!("Error handling DM reply interaction: {e}");
+    error!("Error handling DM reply interaction: {e}");
   }
 
   Ok(())
