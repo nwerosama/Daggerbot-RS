1dc1db5585b35150f79816194a07796bb2c329cd
nwerosama <96593068+nwerosama@users.noreply.github.com>
2025-07-22 21:23:06 +1000

    Switch to mainline Poise

--- src/commands/uptime.rs
+++ src/commands/uptime.rs
@@ -1,34 +1,27 @@
-use crate::{
-  BotError,
-  GIT_COMMIT_BRANCH,
-  GIT_COMMIT_HASH,
-  internals::utils::BOT_VERSION
-};
-
 use {
+  crate::{
+    BotError,
+    GIT_COMMIT_BRANCH,
+    GIT_COMMIT_HASH,
+    internals::utils::BOT_VERSION
+  },
   asahi::utils::{
     format_bytes,
     format_duration,
     os::{
+      get_cpu_info,
       get_kernel_info,
       get_memory,
       get_os_info,
       get_uptime
     }
-  },
-  std::env::var,
-  sysinfo::System
+  }
 };
 
 /// Retrieve host and bot uptimes
 #[poise::command(slash_command)]
 pub async fn uptime(ctx: super::PoiseContext<'_>) -> Result<(), BotError> {
-  let _bot = ctx.http().get_current_user().await.unwrap();
-  let mut sys = System::new_all();
-  sys.refresh_all();
-
-  // Fetch system's processor
-  let cpu = sys.cpus();
+  let bot_name = ctx.cache().current_user().name.clone();
 
   // Fetch system and process memory usage
   let memory = get_memory();
@@ -39,17 +32,17 @@ pub async fn uptime(ctx: super::PoiseContext<'_>) -> Result<(), BotError> {
   );
 
   // Fetch the node hostname from envvar
-  let docker_node = match var("DOCKER_HOSTNAME") {
+  let hostname = match std::env::var("DOCKER_HOSTNAME") {
     Ok(h) => h.to_string(),
     Err(_) => "DOCKER_HOSTNAME is empty!".to_string()
   };
 
   let stat_msg = [
-    format!("**{} {}** `{GIT_COMMIT_HASH}:{GIT_COMMIT_BRANCH}`", _bot.name, BOT_VERSION.as_str()),
+    format!("**{} {}** `{GIT_COMMIT_HASH}:{GIT_COMMIT_BRANCH}`", bot_name, BOT_VERSION.as_str()),
     format!(">>> System: `{}`", format_duration(get_uptime().system)),
     format!("Process: `{}`", format_duration(get_uptime().process)),
-    format!("Node: `{docker_node}`"),
-    format!("CPU: `{}`", cpu[0].brand()),
+    format!("Node: `{hostname}`"),
+    format!("CPU: `{}`", get_cpu_info()),
     format!("RAM: `{pram}` (`{sram}/{sram_total}`)"),
     format!("OS: `{}`", get_os_info()),
     format!("Kernel: `{}`", get_kernel_info())
