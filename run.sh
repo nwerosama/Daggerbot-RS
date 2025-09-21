9125fde6d81cb576445e802a9a9a26409e577907
nwerosama <96593068+nwerosama@users.noreply.github.com>
2025-09-21 14:29:35 +1000

    Consolidate .env and .env.bot into one

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ENV_FILE=.env.bot
+ENV_FILE=.env
 
 export DOCKER_HOSTNAME=$(hostname)
 export $(grep -v '^#' $ENV_FILE | xargs)
 clear && cargo fmt && RUST_LOG=debug cargo run daggerbotbeta
-# clear && gdb -return-child-result -batch -ex run -ex thread apply all bt -ex quit --args target/debug/daggerbot daggerbotbeta
 unset DOCKER_HOSTNAME
 unset $(grep -v '^#' $ENV_FILE | cut -d= -f1)
